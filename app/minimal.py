193fc7942eb646297217dd4616e730e312c53032
Mark Thomas <markmedia+github@zoho.com>
2019-10-17 18:28:48 +0000

    debug: `.gitignore` for django workflow

---
+++ app/minimal.py
@@ -0,0 +1,6 @@
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '3cka(o*a9f)6=ofs8b$^9djre5(fha-5^-@38cjag+v2u87dxa'
+
